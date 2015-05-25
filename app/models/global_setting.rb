20004e089d607177e3d783e71b1dc4ed84e222c4
Tim Heuett <tim.heuett@gmail.com>
2015-05-24 20:09:21 -0700

    add accessors for 'value' attribute, and a ::set method

--- app/models/global_setting.rb
+++ app/models/global_setting.rb
@@ -1,4 +1,37 @@
 class GlobalSetting < ActiveRecord::Base
+  enum datatype: [:string, :integer, :float, :boolean]
+
   validates_presence_of :key, :datatype
   validates :key, uniqueness: true
+
+  def value
+    send(datatype.to_sym)
+  end
+
+  def value=(value)
+    self.datatype = datatype_from_object(value)
+    send("#{datatype}=".to_sym, value)
+    self.value
+  end
+
+  protected
+
+  def datatype_from_object(object)
+    {
+      'String'      => 'string',
+      'Fixnum'      => 'integer',
+      'Float'       => 'float',
+      'TrueClass'   => 'boolean',
+      'FalseClass'  => 'boolean'
+    }[object.class.to_s] || 'string'
+  end
+
+  class << self
+    def set(key, value)
+      setting = find_or_create_by(key: key)
+      setting.value = value
+      setting.save!
+      setting
+    end
+  end
 end
