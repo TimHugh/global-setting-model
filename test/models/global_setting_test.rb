a4b27a61826a75f7360ff1185afd46d43df78dde
Tim Heuett <tim.heuett@gmail.com>
2015-05-24 20:08:51 -0700

    add tests for ::set with all four datatypes

--- test/models/global_setting_test.rb
+++ test/models/global_setting_test.rb
@@ -18,4 +18,28 @@ class GlobalSettingTest < ActiveSupport::TestCase
     setting.datatype = nil
     refute setting.valid?
   end
+
+  def test_set_new_string
+    setting = GlobalSetting.set('email', 'tim@example.com')
+    assert_equal 'tim@example.com',
+                 setting.string
+  end
+
+  def test_set_new_integer
+    setting = GlobalSetting.set('number', 42)
+    assert_equal 42,
+                 setting.integer
+  end
+
+  def test_set_new_float
+    setting = GlobalSetting.set('other-number', 3.14)
+    assert_equal 3.14,
+                 setting.float
+  end
+
+  def test_new_boolean
+    setting = GlobalSetting.set('a-boolean-flag', true)
+    assert_equal true,
+                 setting.boolean
+  end
 end
